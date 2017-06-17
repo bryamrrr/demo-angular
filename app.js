04cb4ba42e230bae3c8915b2c53e1dd2492f834f
Bryam Rodriguez <brodriguezaybar@gmail.com>
2017-06-17 16:57:18 -0500

    Add filter

--- app.js
+++ app.js
@@ -8,22 +8,30 @@ function MiControlador($scope) {
   // Código que define el controller
   // Todo lo que hace el controller
   console.log('Se cargó mi controlador. Todo está funcionando bien');
-  $scope.mensaje = 'Hola, te envio este mensaje desde el más allá';
+  $scope.mensaje = '';
 
   // $scope.listaCompras = ['Yogurt', 'Panes', 'Leche Pura Vida', 'Coca Cola', 'Inka Kola'];
 
   $scope.listaCompras = [
     {
       nombre: 'Yogurt',
-      urlImagen: 'http://biotrendies.com/wp-content/uploads/2015/06/Yogurt-natural.jpg'
+      urlImagen: 'http://biotrendies.com/wp-content/uploads/2015/06/Yogurt-natural.jpg',
+      prioritario: true
     },
     {
       nombre: 'Panes',
-      urlImagen: 'http://www.deollaysarten.com/wp-content/uploads/2014/11/panes.jpg'
+      urlImagen: 'http://www.deollaysarten.com/wp-content/uploads/2014/11/panes.jpg',
+      prioritario: true
     },
     {
       nombre: 'Pura Vida',
-      urlImagen: 'https://img.elcomercio.pe/files/listing_ec_flujo_xx/uploads/2017/06/02/5931d634dfb81.jpeg'
+      urlImagen: 'https://img.elcomercio.pe/files/listing_ec_flujo_xx/uploads/2017/06/02/5931d634dfb81.jpeg',
+      prioritario: false
+    },
+    {
+      nombre: 'Coca Cola',
+      urlImagen: 'https://img.elcomercio.pe/files/listing_ec_flujo_xx/uploads/2017/06/02/5931d634dfb81.jpeg',
+      prioritario: true
     }
   ];
 }
